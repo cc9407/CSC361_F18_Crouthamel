1aa12321081bb71de1f7b2365042ffd1fe6a9b7e
Christian <cc9407@cs.ship.edu>
2018-10-24 13:33:42 -0400

    create util package and make class

---
+++ core/src/com/packtpub/mygdx/retromario/util/CameraHelper.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Christian Crouthamel
+ * Project: Retro Mario
+ * Class: VGP 
+ * Professor: Dr.Girard
+ * Description: Camera helper class
+ */
+
+package com.packtpub.mygdx.retromario.util;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.packtpub.mygdx.retromario.game.objects.AbstractGameObject;
+
+public class CameraHelper {
+
+	
+}
