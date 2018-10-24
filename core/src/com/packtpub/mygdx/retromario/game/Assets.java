fadea21da170dc5d2dedc48cdba84f8575528fa1
Christian <cc9407@cs.ship.edu>
2018-10-23 17:49:35 -0700

    add empty assets class

---
+++ core/src/com/packtpub/mygdx/retromario/game/Assets.java
@@ -0,0 +1,25 @@
+/*
+ * Author: Christian Crouthamel
+ * Project: Retro Mario
+ * Class: VGP 
+ * Professor: Dr.Girard
+ */
+
+package com.packtpub.mygdx.retromario.game;
+
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+
+
+public class Assets {
+
+}
