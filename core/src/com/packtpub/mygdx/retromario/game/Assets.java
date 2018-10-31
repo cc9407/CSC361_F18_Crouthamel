68256100ee679b0c1f31525c01c1ed784a0f7712
Christian <cc9407@cs.ship.edu>
2018-10-31 13:51:53 -0400

    add constants and small fixes to asset

--- core/src/com/packtpub/mygdx/retromario/game/Assets.java
+++ core/src/com/packtpub/mygdx/retromario/game/Assets.java
@@ -14,12 +14,9 @@ import com.badlogic.gdx.assets.AssetErrorListener;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.Disposable;
+import com.packtpub.mygdx.retromario.util.Constants;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
+
 
 
 public class Assets implements Disposable, AssetErrorListener {
@@ -29,7 +26,6 @@ public class Assets implements Disposable, AssetErrorListener {
 	public static final Assets instance = new Assets();
 	//asset manager object
 	private AssetManager assetManager;
-	public AssetFonts fonts;
 	
 	/*
 	 * singleton: prevent instantiation from other classes
@@ -43,15 +39,18 @@ public class Assets implements Disposable, AssetErrorListener {
 	 */
 	public void init(AssetManager assetManager) {
 		
+		this.assetManager = assetManager;
 		//set asset manager error handler
 		assetManager.setErrorListener(this);
+		
+		//load texture atlas
+		assetManager.load(Constants.TEXTURE_ATLAS_OBJECTS, TextureAtlas.class);
 		
 		//start loading assets and wait until finished
 		assetManager.finishLoading();
 		Gdx.app.debug(TAG, "# of assets loaded: " + assetManager.getAssetNames().size );
 		
-		//create game resource objects
-		fonts = new AssetFonts();
+		
 	}
 	
 	/*
@@ -62,9 +61,6 @@ public class Assets implements Disposable, AssetErrorListener {
 	@Override
 	public void dispose() {
 		assetManager.dispose();
-		fonts.defaultSmall.dispose();
-		fonts.defaultNormal.dispose();
-		fonts.defaultBig.dispose();
 	}
 	
 	/*
@@ -86,34 +82,27 @@ public class Assets implements Disposable, AssetErrorListener {
 	}
 	
 	/*
-	 * asset fonts class
+	 *  Builds the rock platforms based on its edges around each central piece
 	 */
-	public class AssetFonts {
-		public final BitmapFont defaultSmall;
-		public final BitmapFont defaultNormal;
-		public final BitmapFont defaultBig;
-		
-		public AssetFonts () {
-			// create three fonts using Libgdx's 15px bitmap font
-			defaultSmall = new BitmapFont(
-			Gdx.files.internal("images/arial-15.fnt"), true);
-			defaultNormal = new BitmapFont(
-			Gdx.files.internal("images/arial-15.fnt"), true);
-			defaultBig = new BitmapFont(
-			Gdx.files.internal("images/arial-15.fnt"), true);
-			
-			// set font sizes
-			defaultSmall.getData().setScale(0.75f);
-			defaultNormal.getData().setScale(1.0f);
-			defaultBig.getData().setScale(2.0f);
-			
-			// enable linear texture filtering for smooth fonts
-			defaultSmall.getRegion().getTexture().setFilter(
-			TextureFilter.Linear, TextureFilter.Linear);
-			defaultNormal.getRegion().getTexture().setFilter(
-			TextureFilter.Linear, TextureFilter.Linear);
-			defaultBig.getRegion().getTexture().setFilter(
-			TextureFilter.Linear, TextureFilter.Linear);
+	public class AssetRock {
+		public final AtlasRegion edge;
+		public final AtlasRegion middle;
+
+		public AssetRock (TextureAtlas atlas) {
+			edge = atlas.findRegion("rock_edge");
+			middle = atlas.findRegion("rock_middle");
 		}
 	}
+
+	/*
+	 *  This inner class contains a member variable called "head"
+	 * and will display the bunny head
+	 */
+	public class AssetMario {
+		public final AtlasRegion head;
+
+		public AssetMario (TextureAtlas atlas) {
+			head = atlas.findRegion("Mario");
+			}
+		}
 }
