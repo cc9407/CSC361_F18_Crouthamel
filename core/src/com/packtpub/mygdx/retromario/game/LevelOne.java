68f221a5926ac66c912f230f526f197d51cdce2f
Christian <cc9407@cs.ship.edu>
2018-11-07 14:36:11 -0500

    create level class

---
+++ core/src/com/packtpub/mygdx/retromario/game/LevelOne.java
@@ -0,0 +1,11 @@
+package com.packtpub.mygdx.retromario.game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
+import com.packtpub.mygdx.retromario.game.objects.AbstractGameObject;
+
+public class LevelOne 
+{
+
+}
