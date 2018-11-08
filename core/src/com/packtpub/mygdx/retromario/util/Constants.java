a111d003aed91eb09ea066e9f3e95dcd0ec0f0a9
Christian <cc9407@cs.ship.edu>
2018-11-07 19:49:15 -0800

    delete one character

--- core/src/com/packtpub/mygdx/retromario/util/Constants.java
+++ core/src/com/packtpub/mygdx/retromario/util/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 	public static final String TEXTURE_ATLAS_OBJECTS = "images/retromario.atlas";
 	
 	// Location of image file for level 01
-	public static final String LEVEL_01 = "levels/level-01.png";
+	public static final String LEVEL_01 = "level/level-01.png";
 	
 	// Amount of extra lives at level start
 		public static final int LIVES_START = 3;
